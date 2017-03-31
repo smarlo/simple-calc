13ee8ed9ef1e8eb3dd38a62e68fa2a61a7392a4a
Sabrina Niklaus <sabrinamarlo@D-173-250-135-229.dhcp4.washington.edu>
2017-03-30 18:59:04 -0700

    initial commit

---
+++ SimpleCalc/SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Sabrina Niklaus on 3/30/17.
+//  Copyright © 2017 Sabrina Niklaus. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
